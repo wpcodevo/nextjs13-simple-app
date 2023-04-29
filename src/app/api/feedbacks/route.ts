242eb0cf14db28ae2953a1003adac3f61a27818f
CODEVO <contact@codevoweb.com>
2023-04-29 16:10:23 +0000

    updated

--- src/app/api/feedbacks/route.ts
+++ src/app/api/feedbacks/route.ts
@@ -2,24 +2,35 @@ import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const page_str = request.nextUrl.searchParams.get("page");
-  const limit_str = request.nextUrl.searchParams.get("limit");
+  try {
+    const page_str = request.nextUrl.searchParams.get("page");
+    const limit_str = request.nextUrl.searchParams.get("limit");
 
-  const page = page_str ? parseInt(page_str, 10) : 10;
-  const limit = limit_str ? parseInt(limit_str, 10) : 1;
-  const skip = (page - 1) * limit;
+    const page = page_str ? parseInt(page_str, 10) : 10;
+    const limit = limit_str ? parseInt(limit_str, 10) : 1;
+    const skip = (page - 1) * limit;
 
-  const feedbacks = await prisma.feedback.findMany({
-    skip,
-    take: limit,
-  });
+    const feedbacks = await prisma.feedback.findMany({
+      skip,
+      take: limit,
+    });
 
-  let json_response = {
-    status: "success",
-    results: feedbacks.length,
-    feedbacks,
-  };
-  return NextResponse.json(json_response);
+    let json_response = {
+      status: "success",
+      results: feedbacks.length,
+      feedbacks,
+    };
+    return NextResponse.json(json_response);
+  } catch (error: any) {
+    let error_response = {
+      status: "error",
+      message: error.message,
+    };
+    return new NextResponse(JSON.stringify(error_response), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 }
 
 export async function POST(request: Request) {
