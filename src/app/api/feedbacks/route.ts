e9739e37bbc40573b296df84b7af636d0a1b7a2d
CODEVO <contact@codevoweb.com>
2023-05-01 09:30:55 +0000

    updated

--- src/app/api/feedbacks/route.ts
+++ src/app/api/feedbacks/route.ts
@@ -6,8 +6,8 @@ export async function GET(request: NextRequest) {
     const page_str = request.nextUrl.searchParams.get("page");
     const limit_str = request.nextUrl.searchParams.get("limit");
 
-    const page = page_str ? parseInt(page_str, 10) : 10;
-    const limit = limit_str ? parseInt(limit_str, 10) : 1;
+    const page = page_str ? parseInt(page_str, 10) : 1;
+    const limit = limit_str ? parseInt(limit_str, 10) : 10;
     const skip = (page - 1) * limit;
 
     const feedbacks = await prisma.feedback.findMany({
