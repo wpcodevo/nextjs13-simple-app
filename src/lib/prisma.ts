f0fd5dc158fbe5967ddbc944a0dd9ccfa83dbd76
CODEVO <contact@codevoweb.com>
2023-06-18 18:13:25 +0000

    updated

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
 export const prisma =
-  globalForPrisma.prisma ||
+  globalForPrisma.prisma ??
   new PrismaClient({
     log: ["query"],
   });
 
-if (process.env.NODE_ENV != "production") globalForPrisma.prisma;
+if (process.env.NODE_ENV != "production") globalForPrisma.prisma = prisma;
