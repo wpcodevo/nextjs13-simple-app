402577ebc43ea1562edccdd43bfa32bb4171e971
kubil6y <lieqb2@gmail.com>
2023-06-18 19:07:27 +0300

    fix prisma initialization
    See for more:
    
    https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -8,4 +8,6 @@ export const prisma =
     log: ["query"],
   });
 
-if (process.env.NODE_ENV != "production") globalForPrisma.prisma;
+if (process.env.NODE_ENV != "production") {
+    globalForPrisma.prisma = prisma;
+}
